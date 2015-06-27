ffc4fcec843f487bc1e28e8218e40b9eee986844
Erik Weitenberg <Erik@Erik7>
2015-06-27 22:30:20 +0200

    some basic stuff

---
+++ tabletop.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "log"
+    "golang.org/x/mobile/app"
+)
+
+func main() {
+    app.Run(app.Callbacks{
+        Draw: draw,
+    })
+}
+
+func draw() {
+    log.Print("In draw loop, can call OpenGL.")
+}
